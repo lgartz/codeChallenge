3e28fa31731c2df18b4f3340e096cfcecce1385d
lgartzcisf <lzarate@clarisacloud.com>
2024-08-13 10:11:30 -0500

    :tada:commit inicial del desafio

---
+++ src/main/java/com/appgate/codetest/CodeChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.appgate.codetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appgate/codetest/CodeChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appgate.codetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
