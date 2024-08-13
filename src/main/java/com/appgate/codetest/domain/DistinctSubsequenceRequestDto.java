281ccc52b37b154437fcda3fb0a9fab6b9f19c0f
lgartzcisf <lzarate@clarisacloud.com>
2024-08-13 11:21:49 -0500

    :test_tube:se agregar los test fallidos

--- src/main/java/com/appgate/codetest/domain/DistinctSubsequenceRequestDto.java
+++ src/main/java/com/appgate/codetest/domain/DistinctSubsequenceRequestDto.java
@@ -1,8 +1,10 @@
 package com.appgate.codetest.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class DistinctSubsequenceRequestDto {
     private String s;
     private String t;
